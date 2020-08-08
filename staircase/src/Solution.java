a079051b1553d2ac257fdb7a445235b21701675b
A1esandr <ravulmirt@gmail.com>
2020-08-08 22:19:37 +0500

    Staircase

---
+++ staircase/src/Solution.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the staircase function below.
+    static void staircase(int n) {
+        for(int i = 1; i <= n; i++) {
+            StringBuilder sb = new StringBuilder();
+            while (sb.length() < n - i) {
+                sb.append(' ');
+            }
+            while (sb.length() < n) {
+                sb.append('#');
+            }
+            System.out.println(sb.toString());
+        }
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        staircase(n);
+
+        scanner.close();
+    }
+}
